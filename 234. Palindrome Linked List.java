23510a55cab4b64ba5ed22edad9998cc4e416e53
Your Name <you@example.com>
2025-05-28 18:00:48 +0530

    Updated Solution for Problem 234

--- 234. Palindrome Linked List.java
+++ 234. Palindrome Linked List.java
@@ -1,17 +1,38 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         ListNode p=head;
-        int num=0;
+        int size=0;
         while(p!=null) {
-            num=num*10+p.val;
+            size++;
             p=p.next;
         }
-        int temp=num, rev=0;
-        while(temp>0) {
-            rev=rev*10+temp%10;
-            temp/=10;
+        p=head;
+        int i=0;
+        int len=size/2;
+        if(size%2==1) {
+            len=(size-1)/2;
         }
-        if(num==rev) return true;
-        return false;
+        int[] firstHalf=new int[len];
+        int[] secondHalf=new int[len];
+        while(i<len) {
+            firstHalf[i++]=p.val;
+            p=p.next;
+        }
+        i=0;
+        if(size%2==1)
+            p=p.next;
+        while(p!=null) {
+            secondHalf[i++]=p.val;
+            p=p.next;
+        }
+        i=0;
+        int j=len-1;
+        while(i<len && j>=0) {
+            if(firstHalf[i]!=secondHalf[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
     }
 }
